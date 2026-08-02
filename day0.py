cd08c283b18087f81527e17b635876e881bf9cc7
unwantedbuffalo67 <raghavkumarchoudhary001@gmail.com>
2026-08-02 12:22:02 +0530

    Update day0.py

--- day0.py
+++ day0.py
@@ -1,9 +1,1 @@
-Hiii so this is day 0 of learning python and i am starting today!!! 
-So i am learning this through a course on udemy by angela yu the course name is 100 days of code tm python
-I am very excited and will surely make a streak less than 100 days lol.
-I mean who nedds 100 days to complete a 60 hr code L.
-Anyways byee..
 
-  Regards
-  Raghav Kumar Choudhary
-  13/4/2026
